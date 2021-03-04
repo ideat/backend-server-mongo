3a64b87100c6d1f8e44edb92ea5eaff1907c2806
ideat <ideatsoft@gmail.com>
2021-03-04 12:09:18 -0400

    Seccion 12 terminada - backend

--- config/config.js
+++ config/config.js
@@ -1,2 +1,6 @@
 
-module.exports.SEED = '@este-es@-un-seed-dificil'
+module.exports.SEED = '@este-es@-un-seed-dificil';
+
+//Google
+
+module.exports.CLIEN_ID = '9296441154-8e3uc8pmup75ld1orq73kkkdia8i5p3l.apps.googleusercontent.com';
